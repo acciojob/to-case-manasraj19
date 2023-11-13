3081bf67a5c8f809e720305729814005246b05a0
Manas Raj <119037651+manasraj19@users.noreply.github.com>
2023-11-13 22:36:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function toCase(text) {
-  // write your code here
+  if (text.length > 0) {
+    const lowercase = text.toLowerCase();
+    const uppercase = text.toUpperCase();
+    return `${lowercase}-${uppercase}`;
+  } else {
+    return '-';
+  }
 }
 
 // DO not change the code below
-
 const text = prompt("Enter text:");
 alert(toCase(text));
